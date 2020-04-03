d0eaa90cc8ade31505155919bd325967b480b746
wesleymutwiri <wesleymochiemo@gmail.com>
2020-04-03 22:24:46 +0300

    Started evaluating of parsed objects

---
+++ object/object.go
@@ -0,0 +1,31 @@
+package object
+
+import "fmt"
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
+type Boolean struct {
+	Value bool
+}
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
