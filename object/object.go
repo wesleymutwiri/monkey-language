c8fc3f3724d677156fe98366da3da2b9c6719512
wesleymutwiri <wesleymochiemo@gmail.com>
2020-04-11 15:38:42 +0300

    Successfully added error handling for wrong input values in the language

--- object/object.go
+++ object/object.go
@@ -7,7 +7,12 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
+
+type Error struct {
+	Message string
+}
 
 type ReturnValue struct {
 	Value Object
@@ -45,3 +50,6 @@ func (n *Null) Inspect() string { return "null" }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
 
 func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
