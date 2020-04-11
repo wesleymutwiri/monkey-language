f3c7838b19b3b4a9db5885cbe41fc971724a5f41
wesleymutwiri <wesleymochiemo@gmail.com>
2020-04-11 17:02:52 +0300

    Successfully added let statements to store values

--- object/object.go
+++ object/object.go
@@ -10,6 +10,10 @@ const (
 	ERROR_OBJ        = "ERROR"
 )
 
+type Environment struct {
+	store map[string]Object
+}
+
 type Error struct {
 	Message string
 }
@@ -53,3 +57,18 @@ func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+func NewEnvironment() *Environment {
+	s := make(map[string]Object)
+	return &Environment{store: s}
+}
+
+func (e *Environment) Get(name string) (Object, bool) {
+	obj, ok := e.store[name]
+	return obj, ok
+}
+
+func (e *Environment) Set(name string, val Object) Object {
+	e.store[name] = val
+	return val
+}
