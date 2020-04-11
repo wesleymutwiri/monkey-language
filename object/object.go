2cc83b1ccc6ac4967ec6c83bbc2a6b6b7da4186e
wesleymutwiri <wesleymochiemo@gmail.com>
2020-04-11 21:04:45 +0300

    Successfully evaluted functions and creating local variables

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
 
 const (
 	INTEGER_OBJ      = "INTEGER"
@@ -8,11 +13,18 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
-type Environment struct {
-	store map[string]Object
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
 }
+
+// type Environment struct {
+// 	store map[string]Object
+// }
 
 type Error struct {
 	Message string
@@ -58,17 +70,36 @@ func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
-func NewEnvironment() *Environment {
-	s := make(map[string]Object)
-	return &Environment{store: s}
-}
+// func NewEnvironment() *Environment {
+// 	s := make(map[string]Object)
+// 	return &Environment{store: s}
+// }
 
-func (e *Environment) Get(name string) (Object, bool) {
-	obj, ok := e.store[name]
-	return obj, ok
-}
+// func (e *Environment) Get(name string) (Object, bool) {
+// 	obj, ok := e.store[name]
+// 	return obj, ok
+// }
 
-func (e *Environment) Set(name string, val Object) Object {
-	e.store[name] = val
-	return val
+// func (e *Environment) Set(name string, val Object) Object {
+// 	e.store[name] = val
+// 	return val
+// }
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
