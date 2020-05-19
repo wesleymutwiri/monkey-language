8916752a8ba8579bea9fa466cc3d841759df8b58
wesleymutwiri <wesleymochiemo@gmail.com>
2020-05-20 00:54:58 +0300

    successfully defined bytecode format and pushes integers to the stack

--- vm/vm.go
+++ vm/vm.go
@@ -1,6 +1,7 @@
 package vm
 
 import (
+	"fmt"
 	"monkey/code"
 	"monkey/compiler"
 	"monkey/object"
@@ -38,8 +39,26 @@ func (vm *VM) Run() error {
 		op := code.Opcode(vm.instructions[ip])
 
 		switch op {
+		case code.OpConstant:
+			constIndex := code.ReadUint16(vm.instructions[ip+1:])
+			ip += 2
 
+			err := vm.push(vm.constants[constIndex])
+			if err != nil {
+				return err
+			}
 		}
 	}
+	return nil
+}
+
+func (vm *VM) push(o object.Object) error {
+	if vm.sp >= StackSize {
+		return fmt.Errorf("stack overflow")
+	}
+
+	vm.stack[vm.sp] = o
+	vm.sp++
+
 	return nil
 }
